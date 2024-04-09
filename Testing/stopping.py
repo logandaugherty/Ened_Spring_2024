8d927b422fe80cd594f61af7b903fa98bfacd0e8
meredith j bartel <98117031+meredithbartel@users.noreply.github.com>
2024-04-09 12:15:47 -0500

    ny
    j

--- Testing/stopping.py
+++ Testing/stopping.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
-from ev3dev2.sensor.lego import UltrasonicSensor
+from ev3dev2.sensor.lego import UltrasonicSensor, largemoter
 from ev3dev2.sensor import INPUT_1
-
+from time import sleep
 sensor = UltrasonicSensor(INPUT_1)
+motor = LargeMotor(OUTPUT_D)
+distance_in = ultrasonic_sensor.distance_inches
+
+if distance_in < 4:
+    motor.stop_action = 'brake'
+    motor.stop()
+
